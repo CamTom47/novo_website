da58e2abb06104d25cd4bbc0d3035d0310e19177
Cameron Thomas <CamThomas47@gmail.com>
2026-05-19 15:06:23 -0400

    added route conditional styling to navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 /** -------------------------MODULES------------------------- **/
-import React, { useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 
 /** -------------------------COMPONENTS------------------------- **/
 
@@ -11,13 +11,20 @@ import { Link, useParams } from "react-router-dom";
 const Navbar = (): React.JSX.Element => {
 	/** -------------------------STATE------------------------- **/
 	const [isNavOpen, setIsNavOpen] = useState<boolean>(false);
+
+	const location = useLocation()
+	console.log('location', location)
 	/** -------------------------FUNCTIONS------------------------- **/
-
-	/** -------------------------EFFECTS------------------------- **/
-
 	const toggleIsNavOpen = () => {
 		setIsNavOpen(!isNavOpen);
 	};
+
+	/** -------------------------EFFECTS------------------------- **/
+
+	useEffect(() => {
+
+	}, [])
+	
 
 	return (
 		<div className='relative grid grid-cols-3 w-full h-fit justify-center items-center py-12 bg-white xl:flex xl:flex-col xl:h-fit xl:py-8 xl:max-h-70'>
@@ -44,27 +51,27 @@ const Navbar = (): React.JSX.Element => {
 			</a>
 			<div
 				className={`absolute z-999 top-32 h-fit bg-white w-full flex flex-col items-center gap-x-6 font-light text-light-gray justify-center duration-300 ${!isNavOpen ? "opacity-0 xl:opacity-100" : "opacity-100"} [&_a]:h-fit [&_a]:py-4 [&_a]:w-full [&_a]:flex [&_a]:px-4 xl:gap-x-8 xl:relative xl:w-1/4 xl:grid xl:grid-cols-4 xl:[&_a]:col-span-1 xl:[&_a]:place-self-center xl:top-0 xl:[&_a]:justify-center xl:[&_a]:w-fit`}>
-				<Link onClick={toggleIsNavOpen} to={"/shop"}>
+				<NavLink onClick={toggleIsNavOpen} to={"/shop"} className={location.pathname === '/shop' ? 'font-semibold' : ''}>
 					Shop
-				</Link>
-				<Link onClick={toggleIsNavOpen} to={"/about"}>
+				</NavLink>
+				<NavLink onClick={toggleIsNavOpen} to={"/about"} className={location.pathname === '/about' ? 'font-semibold' : ''}>
 					About
-				</Link>
-				<Link onClick={toggleIsNavOpen} to={"/gallery"}>
+				</NavLink>
+				<NavLink onClick={toggleIsNavOpen} to={"/gallery"} className={location.pathname === '/gallery' ? 'font-semibold' : ''}>
 					Gallery
-				</Link>
-				<Link onClick={toggleIsNavOpen} to={"/contact"}>
+				</NavLink>
+				<NavLink onClick={toggleIsNavOpen} to={"/contact"} className={location.pathname === '/contact' ? 'font-semibold' : ''}>
 					Contact
-				</Link>
+				</NavLink>
 			</div>
-			<Link to={"/cart"} className='hidden xl:flex'>
+			<NavLink to={"/cart"} className='hidden xl:flex'>
 				<svg
 					className='absolute top-12 right-12 size-6 fill-light-gray'
 					xmlns='http://www.w3.org/2000/svg'
 					viewBox='0 0 640 512'>
 					<path d='M24-16C10.7-16 0-5.3 0 8S10.7 32 24 32l45.3 0c3.9 0 7.2 2.8 7.9 6.6l52.1 286.3c6.2 34.2 36 59.1 70.8 59.1L456 384c13.3 0 24-10.7 24-24s-10.7-24-24-24l-255.9 0c-11.6 0-21.5-8.3-23.6-19.7l-5.1-28.3 303.6 0c30.8 0 57.2-21.9 62.9-52.2L568.9 69.9C572.6 50.2 557.5 32 537.4 32l-412.7 0-.4-2c-4.8-26.6-28-46-55.1-46L24-16zM208 512a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm224 0a48 48 0 1 0 0-96 48 48 0 1 0 0 96z' />
 				</svg>
-			</Link>
+			</NavLink>
 		</div>
 	);
 };
