8d727b37cc606c17c032a55a90e348fea07fa778
Cameron Thomas <CamThomas47@gmail.com>
2026-06-23 06:50:15 -0400

    reconfiguring checkout form

--- frontend/src/components/ShippingForm.tsx
+++ frontend/src/components/ShippingForm.tsx
@@ -1,19 +1,20 @@
 /** -------------------------MODULES------------------------- **/
 import React, { useCallback, useEffect } from "react";
-import { useForm } from "react-hook-form";
-import type { SubmitHandler } from "react-hook-form";
+import type { UseFormRegister, SubmitHandler, FieldErrors } from "react-hook-form";
 import Field from "./Field";
 
 interface FormData {
-	firstName: string;
-	lastName: string;
-	phoneNumber: string;
-	email: string;
-	street: string;
-	city: string;
-	state: string;
-	zipCode: string;
-	country: string;
+	shippingAddress: {
+		firstName: string;
+		lastName: string;
+		phoneNumber: string;
+		email: string;
+		street: string;
+		city: string;
+		state: string;
+		zipCode: string;
+		country: string;
+	};
 }
 
 /** -------------------------COMPONENTS------------------------- **/
@@ -22,64 +23,48 @@ interface FormData {
 
 /** -------------------------INTERFACES------------------------- **/
 interface ShippingFormProps {
-	isCollectingFormData: boolean;
-	collectData: (data: FormData) => void;
+	register: UseFormRegister<FormData>;
+	errors: FieldErrors<FormData>;
 }
 
-const ShippingForm = ({ isCollectingFormData, collectData }: ShippingFormProps): React.JSX.Element => {
+const ShippingForm = ({ register, errors }: ShippingFormProps): React.JSX.Element => {
 	/** -------------------------STATE------------------------- **/
 
 	/** -------------------------FUNCTIONS------------------------- **/
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm<FormData>();
-
-	const onSubmit: SubmitHandler<FormData> = useCallback(
-		(data: FormData) => {
-			collectData(data);
-		},
-		[collectData],
-	);
 
 	/** -------------------------EFFECTS------------------------- **/
 
-	useEffect(() => {
-		if (isCollectingFormData) handleSubmit(onSubmit, (errors) => console.log('what', errors))();
-	}, [isCollectingFormData, handleSubmit, onSubmit]);
-
 	return (
-		<form onSubmit={handleSubmit(onSubmit)} className={"grid grid-cols-2 w-full h-fit rounded gap-x-12 gap-y-8"}>
+		<div className={"grid grid-cols-2 w-full h-fit rounded gap-x-12 gap-y-8"}>
 			<h2 className='col-span-2 text-md border-b'>Shipping Address</h2>
-			<Field label={"First Name"} error={errors.firstName} cols={1}>
-				<input {...register("firstName", { required: true })} type='text' name="firstName" />
+			<Field label={"First Name"} error={errors.shippingAddress?.firstName} cols={1}>
+				<input {...register("shippingAddress.firstName", { required: true })} type='text' name='firstName' />
 			</Field>
 			<Field label={"Last Name"} error={errors.lastName} cols={1}>
-				<input {...register("lastName", { required: true })} type='text' name="lastName" />
+				<input {...register("shippingAddress.lastName", { required: true })} type='text' name='lastName' />
 			</Field>
 			<Field label={"Phone Number"} error={errors.phoneNumber}>
-				<input {...register("phoneNumber", { required: true })} type='text' name='phoneNumber' />
+				<input {...register("shippingAddress.phoneNumber", { required: true })} type='text' name='phoneNumber' />
 			</Field>
 			<Field label={"Email"} error={errors.email}>
-				<input {...register("email", { required: true })} type='text' name='email' />
+				<input {...register("shippingAddress.email", { required: true })} type='text' name='email' />
 			</Field>
 			<Field label={"Street"} error={errors.street}>
-				<input {...register("street", { required: true })} type='text' name="street" />
+				<input {...register("shippingAddress.street", { required: true })} type='text' name='street' />
 			</Field>
 			<Field label={"City"} error={errors.city} cols={1}>
-				<input {...register("city", { required: true })} type='text' name='city' />
+				<input {...register("shippingAddress.city", { required: true })} type='text' name='city' />
 			</Field>
 			<Field label={"State"} error={errors.state} cols={1}>
-				<input {...register("state", { required: true })} type='text' name="state" />
+				<input {...register("shippingAddress.state", { required: true })} type='text' name='state' />
 			</Field>
 			<Field label={"Zip Code"} error={errors.zipCode} cols={1}>
-				<input {...register("zipCode", { required: true })} type='text' name='zipCode' />
+				<input {...register("shippingAddress.zipCode", { required: true })} type='text' name='zipCode' />
 			</Field>
 			<Field label={"Country"} error={errors.country} cols={1}>
-				<input {...register("country", { required: true })} type='text' name='country' />
+				<input {...register("shippingAddress.country", { required: true })} type='text' name='country' />
 			</Field>
-		</form>
+		</div>
 	);
 };
 export default ShippingForm;
--- frontend/src/pages/CheckoutPage.tsx
+++ frontend/src/pages/CheckoutPage.tsx
@@ -1,10 +1,12 @@
 /** -------------------------MODULES------------------------- **/
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import type { SubmitHandler } from "react-hook-form";
+
+/** -------------------------COMPONENTS------------------------- **/
 import ShippingForm from "../components/ShippingForm";
 import OrderSummary from "../components/OrderSummary";
 import BillingForm from "../components/BillingForm";
-
-/** -------------------------COMPONENTS------------------------- **/
 
 /** -------------------------STYLES------------------------- **/
 
@@ -13,38 +15,42 @@ import BillingForm from "../components/BillingForm";
 const CheckoutPage = (): React.JSX.Element => {
 	/** -------------------------STATE------------------------- **/
 	const [currentStep, setCurrentStep] = useState<number>(0);
-	const [isCollectingFormData, setIsCollectingFormData] = useState<boolean>(false);
 
 	/** -------------------------FUNCTIONS------------------------- **/
 
-	const handleSubmit = () => {
-		setIsCollectingFormData(true);
-		setTimeout(() => {
-			setIsCollectingFormData(false);
-		}, 2000);
-	};
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormData>();
 
-	const collectData = (formData) => {
-		console.log("form data", formData);
+	const onSubmit: SubmitHandler<FormData> = (data: FormData) => {
+		console.log("formData", data);
+		// collectData(data);
 	};
 
 	/** -------------------------EFFECTS------------------------- **/
 
 	return (
-		<div className='grid grid-cols-2 gap-x-12 items-start bg-beige w-full xl:px-100 xl:py-12 xl:h-fit'>
-			<div className='flex flex-col gap-y-12'>
-				<ShippingForm collectData={collectData} isCollectingFormData={isCollectingFormData}></ShippingForm>
-				<BillingForm collectData={collectData} isCollectingFormData={isCollectingFormData}></BillingForm>
-			</div>
+		<div className='grid grid-cols-2 gap-x-12 items-start bg-beige w-full xl:px-80 2xl:px-100 xl:py-12 xl:h-fit'>
+			<form
+				onSubmit={handleSubmit(onSubmit, (errors) => console.log(errors))}
+				className='col-span-1 flex flex-col gap-y-12'>
+				<ShippingForm register={register} errors={errors}></ShippingForm>
+				{/* <BillingForm
+					register={register}
+					collectData={collectData}
+					isCollectingFormData={isCollectingFormData}></BillingForm> */}
+				<button className='relative h-10 mt-12 place-self-start' onClick={handleSubmit}>
+					<img src='/assets/button_clay.svg' alt='Novo Shop Link' className='w-full h-full' />
+					<p
+						className='absolute flex items-center justify-center w-full h-full text-lg top-0 left-0 text-nowrap text-clay '
+						style={{ fontFamily: "cheap-pine-sans, sans-serif", fontStyle: "normal", fontWeight: 400 }}>
+						Submit Order
+					</p>
+				</button>
+			</form>
 			<OrderSummary></OrderSummary>
-			<button className='relative h-10 mt-12 place-self-start' onClick={handleSubmit}>
-				<img src='/assets/button_clay.svg' alt='Novo Shop Link' className='w-full h-full' />
-				<p
-					className='absolute flex items-center justify-center w-full h-full text-lg top-0 left-0 text-nowrap text-clay '
-					style={{ fontFamily: "cheap-pine-sans, sans-serif", fontStyle: "normal", fontWeight: 400 }}>
-					Submit Order
-				</p>
-			</button>
 		</div>
 	);
 };
